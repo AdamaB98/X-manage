b7c6970faca333dafc33eac296efdc26e9774a3a
Adama <a.oumarou@alustudent.com>
2022-12-14 21:41:08 +0100

    progress

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,27 @@
 
+import { useState } from "react";
 import "./login.css";
 
 
 function Login() {
+    const [username, setUsername] = useState(" ");
+    const [password, setPassword] = useState(" ");
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+
+
+    }
 
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="login-container">
                 <div className="logo">
                     <h3>Company logo</h3>
                 </div>
                 <h1>CONNEXION</h1>
-                <input  className="username" type="email" placeholder="Username"/>
-                <input className="password" type="password"  placeholder="Password"/>
+                <input  value ={username} onChange={(e => setUsername.target.value)} className="username" type="text" placeholder="Username"/>
+                <input value = {password} onChange={(e => setPassword.target.value)} className="password" type="password"  placeholder="Password"/>
                 <button className="login-btn" type="submit" value="Submit">Login</button>
             </div>
         </form>
